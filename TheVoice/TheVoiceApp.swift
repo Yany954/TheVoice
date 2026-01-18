18f496a7df7c906ce280074d5d8a269df0458aff
Yany954 <yanygonzalezyepez@gmail.com>
2026-01-18 18:56:51 -0500

    Initial Commit

---
+++ TheVoice/TheVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheVoiceApp.swift
+//  TheVoice
+//
+//  Created by Yany Gonzalez Yepez on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct TheVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheVoiceTests/TheVoiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheVoiceTests.swift
+//  TheVoiceTests
+//
+//  Created by Yany Gonzalez Yepez on 1/18/26.
+//
+
+import Testing
+
+struct TheVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
